4880df2e7f5ffb85ba3e55593f3ac826c1a04a7e
Александр Земляк <zemlyaksanya@mail.ru>
2021-10-21 10:33:27 +0300

    Add function SecondsInMinute in Datetime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,18 @@
 ﻿
+function IsLeapYear(y:integer):boolean;
+begin
+  Assert(y > 0, 'y > 0');
+  if y.Divs(4) then 
+  begin
+    Result:= True;
+    if not y.Divs(400) and y.Divs(100) then 
+      Result:= False;
+  end
+  else 
+    Result:= False;
+end;
 
+function SecondsInMinute():= 60;
 
 begin
   
